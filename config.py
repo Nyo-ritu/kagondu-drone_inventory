6ffedd9f021b86a40adda6317493a0f8a950410a
nyo-ritu <kagondu.wai@gmail.com>
2021-08-06 12:53:49 -0400

    2nd commit

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
     # eventually will use hidden variable items - but for now, we'll leave them exposed in config
 
     SECRET_KEY="You will never guess...."
-    DEPLOY_DATABASE_URL = os.environ.get('DEPLOY_DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
 
     # Above line (17) removes unnecessary output on terminal.
